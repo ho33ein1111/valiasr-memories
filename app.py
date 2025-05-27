2e1adc4958a74d5644b1130d7a09fe6e214a8224
ho33ein1111 <60539470+ho33ein1111@users.noreply.github.com>
2025-05-27 09:56:21 +0330

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 creds_dict = json.loads(st.secrets["GSPREAD_SA_JSON"])
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
-sheet = client.open(st.secrets["SHEET_NAME"]).sheet1
+sheet = client.open(st.secrets["SHEET_NAME"]).worksheet("valiasr_memories")
 
 # Load data
 st.set_page_config(layout="wide")
